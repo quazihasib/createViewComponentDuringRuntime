146811cf83df2c819e0039762dab6faacb146416
cafferidwan <quazi.hasib13@gmail.com>
2014-03-03 11:25:25 +0600

    first commit

---
+++ src/com/example/createviewcomponents/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.createviewcomponents;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener 
+{
+	LinearLayout llMain;
+	RadioGroup rgGravity;
+	EditText etName;
+	Button btnCreate;
+	Button btnClear;
+
+	int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		llMain = (LinearLayout) findViewById(R.id.llMain);
+		rgGravity = (RadioGroup) findViewById(R.id.rgGravity);
+		etName = (EditText) findViewById(R.id.etName);
+
+		btnCreate = (Button) findViewById(R.id.btnCreate);
+		btnCreate.setOnClickListener(this);
+
+		btnClear = (Button) findViewById(R.id.btnClear);
+		btnClear.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		switch (v.getId())
+		{
+		case R.id.btnCreate:
+			
+			// creating LayoutParams with width and height defined by content
+			LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(
+					wrapContent, wrapContent);
+			
+			// a variable to store alignment, will be LEFT by default
+			int btnGravity = Gravity.LEFT;
+			
+			// define which RadioButton is checked and fill in bntGravity correspondingly
+			switch (rgGravity.getCheckedRadioButtonId())
+			{
+			case R.id.rbLeft:
+				btnGravity = Gravity.LEFT;
+				break;
+			case R.id.rbCenter:
+				btnGravity = Gravity.CENTER_HORIZONTAL;
+				break;
+			case R.id.rbRight:
+				btnGravity = Gravity.RIGHT;
+				break;
+			}
+			
+			// put the received alignment value into LayoutParams
+			lParams.gravity = btnGravity;
+
+			// create a Button, change text and add it to the LinearLayout
+			Button btnNew = new Button(this);
+			btnNew.setText(etName.getText().toString());
+			llMain.addView(btnNew, lParams);
+
+			break;
+
+		case R.id.btnClear:
+			llMain.removeAllViews();
+			Toast.makeText(this, "Deleted", Toast.LENGTH_SHORT).show();
+			break;
+
+		}
+	}
+
+} 
